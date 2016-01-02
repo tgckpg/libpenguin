50a4b0865e853e748ba492872211dff01b63b2c3
斟酌 鵬兄 <tgckpg@gmail.com>
2016-01-02 18:02:29 +0800

    Moved some class from libwenku8

---
+++ Net.Astropenguin/Net/Astropenguin/Loaders/EventArgs.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+using Net.Astropenguin.Logging;
+
+namespace Net.Astropenguin.Loaders
+{
+	public class DThreadArgs : EventArgs
+	{
+		public string FileLocation { get; private set; }
+		public Guid Id { get; private set; }
+		public DThreadArgs( string SaveLocation, Guid id )
+		{
+			FileLocation = SaveLocation;
+			Id = id;
+		}
+	}
+
+	public class DThreadUpdateArgs : DThreadArgs
+	{
+		public string Desc { get; private set; }
+		public DThreadUpdateArgs( string SaveLocation, Guid id, string desc )
+			: base( SaveLocation, id )
+		{
+			Desc = desc;
+		}
+	}
+
+	public class DTheradCompleteArgs : DThreadArgs
+	{
+		public DTheradCompleteArgs( string SaveLocation, Guid id )
+			: base( SaveLocation, id )
+		{
+		}
+	}
+
+	public class DCycleCompleteArgs : EventArgs 
+	{
+		public DCycleCompleteArgs()
+			: base()
+		{
+		}
+	}
+
+	public class DRequestCompletedEventArgs : EventArgs
+	{
+        public static readonly string ID = typeof( DRequestCompletedEventArgs ).Name;
+
+		private string rString;
+		private Exception RequestException;
+		private byte[] RBytes;
+
+		public string RequestUrl { get; private set; }
+
+		public Exception Error { get; private set; }
+
+		public WebHeaderCollection ResponseHeaders { get; private set; }
+
+
+		public byte[] ResponseBytes
+		{
+			get
+			{
+				// Throw exception if exception exists
+				if ( RequestException != null ) throw RequestException;
+				return RBytes;
+			}
+		}
+
+		public string ResponseString
+		{
+			get
+			{
+				// Throw exception if exception exists
+				if ( RequestException != null ) throw RequestException;
+				if ( rString == null )
+				{
+					rString = Encoding.UTF8.GetString( RBytes, 0, RBytes.Length );
+					if ( !string.IsNullOrEmpty( rString ) && rString[0] == 65279 )
+						// Remove EF BB BF
+						rString = rString.Substring( 1 );
+				}
+				return rString;
+			}
+		}
+
+		public DRequestCompletedEventArgs( WebHeaderCollection RHeader, string Url, byte[] RawBytes )
+		{
+			RequestUrl = Url;
+			RBytes = RawBytes;
+			ResponseHeaders = RHeader;
+		}
+
+		public DRequestCompletedEventArgs( string Url, Exception ex )
+		{
+			// Failed Exception will thrown in Getting ResponseString or ResponseBytes Method
+			Logger.Log( ID, "A WebException occured at " + Url + ": " + ex.Message, LogType.DEBUG );
+
+			RequestUrl = Url;
+			RequestException = ex;
+		}
+	}
+
+	public class DResponseSavedEventArgs : EventArgs
+	{
+		public string SaveLocation { get; private set; }
+		public DResponseSavedEventArgs( string SLocation )
+		{
+			SaveLocation = SLocation;
+		}
+	}
+
+	public class DThreadProgressArgs : DThreadArgs
+	{
+		public long BytesReceived { get; private set; }
+		public long BytesTotal { get; private set; }
+		public int Percentage
+		{
+			get
+			{
+				return Convert.ToInt32( ( Convert.ToDouble( BytesReceived ) / Convert.ToDouble( BytesTotal ) * 100 ) );
+			}
+		}
+		public DThreadProgressArgs( string SaveLocation, long TotalBytesReceived, long TotalBytesToReceive, Guid id )
+			: base( SaveLocation, id )
+		{
+			BytesReceived = TotalBytesReceived;
+			BytesTotal = TotalBytesToReceive;
+			if ( BytesTotal == 0 )
+			{
+				BytesTotal = 1;
+				BytesReceived = 0;
+			}
+		}
+	}
+}
---
+++ Net.Astropenguin/Net/Astropenguin/Loaders/HttpRequest.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+
+using Net.Astropenguin.Helpers;
+using Net.Astropenguin.Logging;
+
+namespace Net.Astropenguin.Loaders
+{
+	public class HttpRequest
+	{
+        public static readonly string ID = typeof( HttpRequest ).Name;
+
+		#region DThread Handlers
+		public delegate void DRequestCompleteHandler( DRequestCompletedEventArgs DArgs );
+
+		private event DRequestCompleteHandler DRequestCompleted;
+
+		public event DRequestCompleteHandler OnRequestComplete
+		{
+			add
+			{
+				DRequestCompleted -= value;
+				DRequestCompleted += value;
+			}
+			remove
+			{
+				DRequestCompleted -= value;
+			}
+		}
+		#endregion
+
+		public HttpStatusCode StatusCode;
+		public bool EN_UITHREAD = true;
+
+        public int Timeout
+        {
+            get { return WCRequest.ContinueTimeout; }
+            set { WCRequest.ContinueTimeout = value; }
+        }
+
+		protected HttpWebRequest WCRequest;
+		protected byte[] PostData;
+
+		public Uri ReqUri;
+
+		public string ContentType
+		{
+			set { WCRequest.ContentType = value; }
+			get { return WCRequest.ContentType; }
+		}
+
+		public long ContentLength
+		{
+			get { return int.Parse( WCRequest.Headers[ HttpRequestHeader.ContentLength ] ); }
+		}
+
+        public string Method
+        {
+            get { return WCRequest.Method; }
+            set { WCRequest.Method = value; }
+        }
+
+		public HttpRequest( Uri RequestUri )
+		{
+			ReqUri = RequestUri;
+			PostData = new byte[0];
+			CreateRequest();
+		}
+
+		virtual protected void CreateRequest()
+		{
+			WCRequest = ( HttpWebRequest ) WebRequest.Create( ReqUri );
+			WCRequest.Method = "GET";
+			WCRequest.Headers[ HttpRequestHeader.UserAgent ] = "libpenguin - HTTPRequest";
+		}
+
+		public void OpenWriteAsync( string DataString )
+		{
+			// Prepare streaming bytes
+			PostData = Encoding.UTF8.GetBytes( DataString );
+
+			OpenWriteAsync();
+		}
+
+		public void OpenWriteAsync()
+		{
+			WCRequest.Headers[ HttpRequestHeader.ContentLength ] = PostData.Length.ToString();
+			WCRequest.BeginGetRequestStream( new AsyncCallback( GetRequestStreamCallback ), WCRequest );
+		}
+
+        public void OpenAsync()
+        {
+            WCRequest.BeginGetResponse( new AsyncCallback( GetResponseCallback ), WCRequest );
+        }
+
+        public void UpdateProto( Uri Url )
+        {
+            ReqUri = Url;
+            CreateRequest();
+        }
+
+		public void OpenAsyncThread( string DataString, bool EnableUIThread )
+		{
+			// Continuous call and preserve contentType
+			string CType = ContentType;
+			CreateRequest();
+			WCRequest.ContentType = CType;
+			EN_UITHREAD = EnableUIThread;
+			OpenWriteAsync( DataString );
+		}
+
+		public void Stop()
+		{
+			WCRequest.Abort();
+		}
+
+		private void GetRequestStreamCallback( IAsyncResult AsyncResult )
+		{
+			try
+			{
+				HttpWebRequest Request = ( HttpWebRequest ) AsyncResult.AsyncState;
+
+				// End Request
+				Stream POSTWriter = Request.EndGetRequestStream( AsyncResult );
+
+				// Write request stream.
+				POSTWriter.Write( PostData, 0, PostData.Length );
+				POSTWriter.Dispose();
+
+				// GetResponse
+				Request.BeginGetResponse( new AsyncCallback( GetResponseCallback ) , Request );
+			}
+			catch ( Exception )
+			{
+				// MessageBox.Show( ex.ToString() );
+			}
+		}
+
+		private void GetResponseCallback( IAsyncResult AsyncResult )
+		{
+			HttpWebRequest Request = ( HttpWebRequest ) AsyncResult.AsyncState;
+			string RefUrl = 0 < PostData.Length
+				// Mostly PostData
+				? Encoding.UTF8.GetString( PostData, 0, PostData.Length )
+				// Rarely GET Requests
+				: ReqUri.ToString()
+				;
+			try
+			{
+				HttpWebResponse Response = ( HttpWebResponse ) Request.EndGetResponse( AsyncResult );
+				StatusCode = Response.StatusCode;
+
+				if ( DRequestCompleted != null )
+				{
+					byte[] rBytes;
+					using ( Stream ResponseStream = Response.GetResponseStream() )
+					{
+						// Read stream in to byte
+						byte[] buffer = new byte[16 * 1024];
+
+						using ( MemoryStream ms = new MemoryStream() )
+						{
+							int read;
+							while ( 0 < ( read = ResponseStream.Read( buffer, 0, buffer.Length ) ) )
+							{
+								ms.Write( buffer, 0, read );
+							}
+							rBytes = ms.ToArray();
+						}
+					}
+					DRequestCompletedEventArgs RArgs
+						= new DRequestCompletedEventArgs( Response.Headers, RefUrl, rBytes );
+					if ( EN_UITHREAD )
+						// Raise event in the Main UI thread
+						Worker.UIInvoke( () => DRequestCompleted( RArgs ) );
+					else
+						DRequestCompleted( RArgs );
+				}
+
+				// Close HttpWebResponse
+				Response.Dispose();
+			}
+			catch ( Exception ex )
+			{
+                if ( EN_UITHREAD )
+                {
+                    Worker.UIInvoke( () =>
+                    {
+                        // Throw Exception to CompletedArgs
+                        DRequestCompleted( new DRequestCompletedEventArgs( RefUrl, ex ) );
+                    } );
+                }
+                else
+                {
+                    DRequestCompleted( new DRequestCompletedEventArgs( RefUrl, ex ) );
+                }
+            }
+		}
+
+	}
+}
