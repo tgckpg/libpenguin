1d8a6c48b245185d5dee03cece7ae3d94ee5a050
斟酌 鵬兄 <tgckpg@gmail.com>
2016-01-07 03:59:06 +0800

    Join Linq Extensions

--- Net.Astropenguin/Net/Astropenguin/Linq.cs
+++ Net.Astropenguin/Net/Astropenguin/Linq.cs
@@ -7,8 +7,34 @@ using System.Threading.Tasks;
 
 namespace Net.Astropenguin.Linq
 {
-    public static class ObservableCollection
+    public static class LinqExtensions
     {
+        public static IEnumerable<TTarget> Construct<TSource, TTarget>( this IEnumerable<TSource> Source, Func<TSource, TTarget> Translator )
+        {
+            int i = 0; int l = Source.Count();
+
+            TTarget[] Translated = new TTarget[ l ];
+            foreach ( TSource Item in Source )
+            {
+                Translated[ i++ ] = Translator( Item );
+            }
+
+            return Translated;
+        }
+
+        public static void Filter<TSource>( this List<TSource> source, Func<TSource, bool> keySelector )
+        {
+            List<TSource> FilteredList = source.Where( keySelector ).ToList();
+
+            source.Clear();
+
+            if( FilteredList != null )
+            foreach ( TSource Item in FilteredList )
+            {
+                source.Add( Item );
+            }
+        }
+
         public static void Sort<TSource, TKey>( this ObservableCollection<TSource> source, Func<TSource, TKey> keySelector )
         {
             List<TSource> sortedList = source.OrderBy( keySelector ).ToList();
--- Net.Astropenguin/Net/Astropenguin/UI/Converters/EnumNameConvorter.cs
+++ Net.Astropenguin/Net/Astropenguin/UI/Converters/EnumNameConvorter.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 
-namespace Net.Astropenguin.Helpers.Converters
+namespace Net.Astropenguin.UI.Converters
 {
     public class EnumNameConvorter : IValueConverter
     {
