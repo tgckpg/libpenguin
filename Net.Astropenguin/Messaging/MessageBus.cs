1de0ceebd94402ee05161a9f2c333c6599ff2b25
斟酌 鵬兄 <tgckpg@gmail.com>
2016-08-11 09:36:31 +0800

    SendUI alias

--- Net.Astropenguin/Messaging/MessageBus.cs
+++ Net.Astropenguin/Messaging/MessageBus.cs
@@ -25,9 +25,19 @@ namespace Net.Astropenguin.Messaging
             }
         }
 
+        public static void SendUI( Type Id, string Content, object Payload = null )
+        {
+            SendUI( new Message( Id, Content, Payload ) );
+        }
+
         public static void SendUI( Message Mesg )
         {
             Helpers.Worker.UIInvoke( () => { DoDelivery?.Invoke( Mesg ); } );
+        }
+
+        public static void Send( Type Id, string Content, object Payload = null )
+        {
+            Send( new Message( Id, Content, Payload ) );
         }
 
         public static void Send( Message Mesg )
