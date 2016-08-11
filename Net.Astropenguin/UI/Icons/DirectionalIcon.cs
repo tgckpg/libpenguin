dac5b64f020515eee3add312c41cbe942dd5d4ac
斟酌 鵬兄 <tgckpg@gmail.com>
2016-08-12 00:28:22 +0800

    Dir Icon should allow both Mirror & Rotation

--- Net.Astropenguin/UI/Icons/DirectionalIcon.cs
+++ Net.Astropenguin/UI/Icons/DirectionalIcon.cs
@@ -6,9 +6,13 @@ namespace Net.Astropenguin.UI.Icons
 {
     public enum Direction
     {
-        Normal
-        , Rotate90, Rotate180, Rotate270
-        , MirrorVertical, MirrorHorizontal, MirrorBoth
+        Normal = 0
+        , Rotate90 = 1
+        , Rotate180 = 2
+        , Rotate270 = 4
+        , MirrorVertical = 8
+        , MirrorHorizontal = 16
+        , MirrorBoth = 32
     }
 
     public abstract class DirectionalIcon : IconBase
@@ -35,27 +39,31 @@ namespace Net.Astropenguin.UI.Icons
 
         protected void DirectionPass()
         {
-            switch( Direction )
+            if ( ( Direction & Direction.MirrorVertical ) != 0 )
             {
-                case Direction.MirrorVertical:
-                    StageTransform.ScaleX *= -1;
-                    break;
-                case Direction.MirrorHorizontal:
-                    StageTransform.ScaleY *= -1;
-                    break;
-                case Direction.MirrorBoth:
-                    StageTransform.ScaleX *= -1;
-                    StageTransform.ScaleY *= -1;
-                    break;
-                case Direction.Rotate90:
-                    StageTransform.Rotation = 90;
-                    break;
-                case Direction.Rotate180:
-                    StageTransform.Rotation = 180;
-                    break;
-                case Direction.Rotate270:
-                    StageTransform.Rotation = 270;
-                    break;
+                StageTransform.ScaleX *= -1;
+            }
+            else if ( ( Direction & Direction.MirrorHorizontal ) != 0 )
+            {
+                StageTransform.ScaleY *= -1;
+            }
+            else if ( ( Direction & Direction.MirrorBoth ) != 0 )
+            {
+                StageTransform.ScaleX *= -1;
+                StageTransform.ScaleY *= -1;
+            }
+
+            if ( ( Direction & Direction.Rotate90 ) != 0 )
+            {
+                StageTransform.Rotation = 90;
+            }
+            else if ( ( Direction & Direction.Rotate180 ) != 0 )
+            {
+                StageTransform.Rotation = 180;
+            }
+            else if ( ( Direction & Direction.Rotate270 ) != 0 )
+            {
+                StageTransform.Rotation = 270;
             }
         }
 
@@ -63,6 +71,6 @@ namespace Net.Astropenguin.UI.Icons
         {
             ( d as DirectionalIcon ).DirectionPass();
         }
-    }
 
+    }
 }
