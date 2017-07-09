a04f0734ba6d02fb747b5c9b0bd9b6e33c324c18
斟酌 鵬兄 <tgckpg@gmail.com>
2017-07-09 22:04:39 +0800

    Delay 0 should skip calling Task.Delay

--- Net.Astropenguin/UI/VerticalStack.cs
+++ Net.Astropenguin/UI/VerticalStack.cs
@@ -118,6 +118,12 @@ namespace Net.Astropenguin.UI
 
 		private async void DelaySizeUpdate()
 		{
+			if ( UpdateDelay == 0 )
+			{
+				UpdateDisplay();
+				return;
+			}
+
 			if ( DLock ) return;
 			DLock = true;
 			await Task.Delay( UpdateDelay );
