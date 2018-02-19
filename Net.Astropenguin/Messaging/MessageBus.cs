e893115cad049917c5ddc7468561ae2f714489ef
斟酌 鵬兄 <tgckpg@gmail.com>
2018-02-19 17:23:27 +0800

    MessageBus should use the weak event pattern

--- Net.Astropenguin/Messaging/MessageBus.cs
+++ Net.Astropenguin/Messaging/MessageBus.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Net.Astropenguin.Messaging
 {
@@ -10,20 +12,10 @@ namespace Net.Astropenguin.Messaging
 	{
 		public delegate void MessageEvent( Message Mesg );
 
-		private static event MessageEvent DoDelivery;
+		private static _Messenger Messenger = new _Messenger();
 
-		public static event MessageEvent OnDelivery
-		{
-			add
-			{
-				DoDelivery -= value;
-				DoDelivery += value;
-			}
-			remove
-			{
-				DoDelivery -= value;
-			}
-		}
+		public static void Subscribe( object target, MessageEvent e ) => Messenger.AddHandler( target, e );
+		public static void Unsubscribe( object target, MessageEvent e ) => Messenger.RemoveHandler( e );
 
 		public static void SendUI( Type Id, string Content, object Payload = null )
 		{
@@ -32,7 +24,7 @@ namespace Net.Astropenguin.Messaging
 
 		public static void SendUI( Message Mesg )
 		{
-			Helpers.Worker.UIInvoke( () => { DoDelivery?.Invoke( Mesg ); } );
+			Helpers.Worker.UIInvoke( () => Messenger.Deliver( Mesg ) );
 		}
 
 		public static void Send( Type Id, string Content, object Payload = null )
@@ -42,7 +34,45 @@ namespace Net.Astropenguin.Messaging
 
 		public static void Send( Message Mesg )
 		{
-			Task.Run( () => { DoDelivery?.Invoke( Mesg ); } );
+			Task.Run( () => Messenger.Deliver( Mesg ) );
+		}
+
+		private class _Messenger
+		{
+			public ConcurrentDictionary<MethodInfo, WeakReference<object>> Handlers;
+
+			public _Messenger()
+			{
+				Handlers = new ConcurrentDictionary<MethodInfo, WeakReference<object>>();
+			}
+
+			public void AddHandler( object Target, MessageEvent Handler )
+			{
+				Handlers[ Handler.GetMethodInfo() ] = new WeakReference<object>( Target );
+			}
+
+			public void RemoveHandler( MessageEvent Handler )
+			{
+				Handlers.TryRemove( Handler.GetMethodInfo(), out WeakReference<object> NOP );
+			}
+
+			public void Deliver( Message Mesg )
+			{
+				foreach ( MethodInfo Handler in Handlers.Keys.ToArray() )
+				{
+					if ( Handlers.TryGetValue( Handler, out WeakReference<object> WeakEvent ) )
+					{
+						if ( WeakEvent.TryGetTarget( out object Target ) )
+						{
+							Handler.Invoke( Target, new object[] { Mesg } );
+						}
+						else
+						{
+							Handlers.TryRemove( Handler, out WeakReference<object> NOP );
+						}
+					}
+				}
+			}
 		}
 
 	}
